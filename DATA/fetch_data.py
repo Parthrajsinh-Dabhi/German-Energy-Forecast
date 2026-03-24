b5d85d4e9ee094d27f16a7e0db66e060b98592c4
Parthrajsinh_16 <parthdabhi207@gmail>
2026-03-24 23:48:32 +0100

    final project update

--- DATA/fetch_data.py
+++ DATA/fetch_data.py
@@ -1,14 +1,1 @@
-import pandas as pd
 
-print("Project setup successful")
-
-data = {
-    "hour":[1,2,3,4,5],
-    "electricity_demand":[50000,52000,51000,53000,54000]
-}
-
-df = pd.DataFrame(data)
-
-df.to_csv("sample_energy_data.csv", index=False)
-
-print("Sample dataset created")
